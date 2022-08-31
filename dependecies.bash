0f05f531a4dfd7dcdae1f37728551612eb6b5c68
caraplana1 <cristianlandaez@gmail.com>
2022-08-30 23:58:41 -0400

    clangd added to dependecies

--- dependecies.bash
+++ dependecies.bash
@@ -4,5 +4,5 @@ sudo cp ./Fonts/'Mononoki-Regular Nerd Font Complete Windows Compatible.ttf' /us
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-sudo apt install ripgrep
+sudo apt install ripgrep clangd
 curl -sL install-node.vercel.app/lts | sudo bash
