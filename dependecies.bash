26a3bc4f0bac6c1357d629525e70990ebb7c02fe
caraplana1 <cristianlandaez@gmail.com>
2022-08-30 21:06:16 -0400

    Conquer of completation added and vim plug as well

--- dependecies.bash
+++ dependecies.bash
@@ -1,4 +1,8 @@
 sudo cp ./Fonts/'Monoid Regular Nerd Font Complete Windows Compatible.ttf' /usr/share/fonts/'Monoid Regular Nerd Font Complete Windows Compatible.ttf'
 sudo cp ./Fonts/'Mononoki-Regular Nerd Font Complete Windows Compatible.ttf' /usr/share/fonts/'Mononoki-Regular Nerd Font Complete Windows Compatible.ttf'
 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 sudo apt install ripgrep
+curl -sL install-node.vercel.app/lts | sudo bash
