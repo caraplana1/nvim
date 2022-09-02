ce3d13ffa140eb904d383226863abd0ccee1cb7a
caraplana1 <cristianlandaez@gmail.com>
2022-09-01 21:36:06 -0400

    Clangd 12 suppoert added

--- Ubuntu-Dependecies.bash
+++ Ubuntu-Dependecies.bash
@@ -3,5 +3,6 @@ sudo cp ./Fonts/'mononoki-Regular Nerd Font Complete Mono.ttf' /usr/share/fonts/
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-sudo apt install ripgrep clangd
+sudo apt install ripgrep clangd-12
+echo "alias clangd=clangd-12" >> ~/.bashrc
 curl -sL install-node.vercel.app/lts | sudo bash
